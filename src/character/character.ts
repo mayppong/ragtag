a5d02ed78a27e25c6e8e10738217cc6c13d55918
May Pongpitpitak <mayppong@ShadowStalker.local>
2017-05-20 12:57:09 -0700

    Create character class

---
+++ src/character/character.ts
@@ -0,0 +1,14 @@
+/**
+ * Base class for all characters
+ *
+ */
+class Character {
+
+  readonly name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+export default Character;
