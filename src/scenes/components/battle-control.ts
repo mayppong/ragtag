fdeb023e51fc259efadcebfee4d1581e89190d61
May Pongpitpitak <me@mayppong.com>
2020-03-03 22:18:17 -0800

    Refactor character controls to draw grid instead.
    Instead of individually drawing each box, we draw an entire grid
    instead. This should hopefully be ore efficient.

--- src/scenes/components/battle-control.ts
+++ src/scenes/components/battle-control.ts
@@ -8,38 +8,48 @@ interface ISize {
 
 class BattleControl extends Phaser.Scene {
   static readonly defaultCharacterControlPositions: Phaser.Types.GameObjects.Graphics.Options[] = [{x: 0, y: 0}, {x: 0, y: 100}, {x: 0, y: 200}, {x: 270, y: 0}, {x: 270, y: 100}, {x: 270, y: 200}];
-
-  readonly position: Phaser.Types.GameObjects.Graphics.Options = {x: 0, y: 660};
-  readonly size: ISize = {height: 300, width: 540};
-  readonly row: number = 3;
-  readonly column: number = 2;
+  static readonly defaultGraphicsConfig: any = {
+    fillStyle: 0x031f4c,
+    outlineStyle: 0xffffff
+  }
+  static readonly characterControlsConfig: any = {
+    originPosition: {x: 0, y: 660},
+    gridSize: {width: 540, height: 300},
+    cellSize: {width: 270, height: 100}, // 3 rows, 2 columns
+    fillColor: 0x031f4c,
+    outlineColor: 0xffffff
+  };
 
   constructor() {
     super({ key: 'BattleControl'});
   }
 
   create(party: CharacterSprite[]) {
-    let graphics = this.add.graphics(this.position);
-    graphics.lineStyle(1, 0xffffff);
-    graphics.fillStyle(0x031f4c, 1);
+    this.drawCharacterControlsGrid(this, BattleControl.characterControlsConfig);
 
-    BattleControl.defaultCharacterControlPositions.forEach((menu) => {
-      this.drawControlsBox(graphics, menu.x, menu.y);
+    party.forEach((hero, index) => {
+      let x = BattleControl.characterControlsConfig.originPosition.x + BattleControl.defaultCharacterControlPositions[index].x;
+      let y = BattleControl.characterControlsConfig.originPosition.y + BattleControl.defaultCharacterControlPositions[index].y;
+      let control = new CharacterControl(this, {x: x, y: y});
+      control.addMenuItem(hero.character.id);
+      this.add.existing(control);
     });
-
-    let menus = this.add.container(this.position.x, this.position.y);
-
-    let heroesMenu = new CharacterControl(this, 1, 10);
-    party.forEach((hero) => {
-      heroesMenu.addMenuItem(hero.character.id)
-    });
-    menus.add(heroesMenu);
   }
 
-  private drawControlsBox(graphics: Phaser.GameObjects.Graphics, x: number, y: number) {
-    graphics.strokeRect(x, y, 270, 100);
-    graphics.fillRect(x, y, 270, 100);
+  private setGraphicsDefaultStyle(graphics: Phaser.GameObjects.Graphics) {
+    graphics.lineStyle(1, BattleControl.defaultGraphicsConfig.outlineStyle);
+    graphics.fillStyle(BattleControl.defaultGraphicsConfig.fillStyle, 1);
     return graphics;
+  }
+
+  private drawCharacterControlsGrid(scene: Phaser.Scene, controlsConfig: any) {
+    let grid = scene.add.grid(controlsConfig.originPosition.x, controlsConfig.originPosition.y,
+        controlsConfig.gridSize.width, controlsConfig.gridSize.height,
+        controlsConfig.cellSize.width, controlsConfig.cellSize.height,
+        controlsConfig.fillColor, 1, controlsConfig.outlineColor)
+      .setOrigin(0,0)
+      .setStrokeStyle(1, controlsConfig.outlineColor);
+    return grid;
   }
 
 }
--- src/scenes/components/battle-controls/character-control.ts
+++ src/scenes/components/battle-controls/character-control.ts
@@ -1,14 +1,19 @@
 class CharacterControl extends Phaser.GameObjects.Container {
 
+  static readonly width: number = 270;
+  static readonly height: number = 100;
+
   readonly scene: Phaser.Scene;
 
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y);
+  constructor(scene: Phaser.Scene, position: Phaser.Types.GameObjects.Graphics.Options) {
+    super(scene, position.x, position.y);
+    this.setInteractive(new Phaser.Geom.Rectangle(0, 0, CharacterControl.width, CharacterControl.height), Phaser.Geom.Rectangle.Contains);
   }
 
-  addMenuItem(index: number, item: string) {
+  addMenuItem(item: string) {
     let menuItem = new Phaser.GameObjects.Text(this.scene, 10, 10, item, { color: '#ffffff', align: 'left', fontSize: '1.5rem'});
     this.add(menuItem);
+    return this;
   }
 
 }
